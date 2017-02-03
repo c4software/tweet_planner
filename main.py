20ec779e3f5d10e74ce1a66c03b322eeb0b07ccd
Valentin Brosseau <c4software@gmail.com>
2017-02-03 23:12:52 +0100

    Add init file and debug flag

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ parser.add_argument('--debug', action='store_true', help='Debug mode')
 parser.add_argument('-a', '--add', help="Add a new tweet to the « queue ».")
 args = parser.parse_args()
 
-def main():
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+def send_tweet():
+    logging.debug("Send Tweet")
     twitter_api = tweeter_auth()
 
 def tweeter_auth():
@@ -21,9 +25,22 @@ def tweeter_auth():
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
+def add_queue():
+    logging.debug("Add tweet to queue")
+    pass
+
+def init_queue():
+    logging.debug("Init queue")
+    open("waiting.txt", 'a').close()
+
 if __name__ == '__main__':
     try:
-        main()
+        if args.init:
+            init_queue()
+        elif args.add:
+            add_queue()
+        else:
+            send_tweet()
     except tweepy.error.TweepError as e:
         print("Twitter error: {0}".format(e))
     except Exception as e:
