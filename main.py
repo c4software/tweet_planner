62eb303cd8caec10d6ecb905d07bce5938253ce1
Valentin Brosseau <c4software@gmail.com>
2017-02-04 21:30:48 +0100

    Add test to exit if queue file is absent, and text length

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 import argparse
 import logging
 import tweepy
+import sys
+import os.path
 
 from secrets import consumer_key, consumer_secret, access_token, access_token_secret
 
-
 parser = argparse.ArgumentParser(description="Tweet planner")
-
 parser.add_argument('-i', '--init', action='store_true', help='Init the « queue » tweets databases.')
 parser.add_argument('--debug', action='store_true', help='Debug mode')
 parser.add_argument('-a', '--add', help="Add a new tweet to the « queue ».")
 args = parser.parse_args()
+
+queue_file_name = queue_file_name
 
 if args.debug:
     logging.basicConfig(level=logging.DEBUG)
@@ -25,20 +27,33 @@ def tweeter_auth():
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
-def add_queue():
+def add_to_queue(text):
     logging.debug("Add tweet to queue")
-    pass
+    if len(text) > 140:
+        logging.error("The text can’t exceed 140 characters")
+        sys.exit()
+    else:
+        with open(queue_file_name, "a") as queue_file:
+            queue_file.write(text)
+
+def check_queue_file():
+    return os.path.isfile(queue_file_name)
 
 def init_queue():
     logging.debug("Init queue")
-    open("waiting.txt", 'a').close()
+    open(queue_file_name, 'a').close()
 
 if __name__ == '__main__':
     try:
+        # Test if queue is present
+        if not args.init and not check_queue_file():
+            logging.error("To use Tweet Planner you must init the queue (use the -i flags)")
+            sys.exit()
+
         if args.init:
             init_queue()
         elif args.add:
-            add_queue()
+            add_to_queue(args.add)
         else:
             send_tweet()
     except tweepy.error.TweepError as e:
